1db0093ababa338495201b9f4c6973d815937288
Roshan852k <roshany852k@gmail.com>
2024-11-08 00:08:41 +0530

    Now we can store password in encrypt form in database successfully.

--- src/main/java/com/roshan/hungry/service/CustomerService.java
+++ src/main/java/com/roshan/hungry/service/CustomerService.java
@@ -4,6 +4,8 @@ import com.roshan.hungry.dto.CustomerRequest;
 import com.roshan.hungry.dto.CustomerLoginRequest;
 import com.roshan.hungry.dto.CustomerResponse;
 import com.roshan.hungry.entity.Customer;
+import com.roshan.hungry.helper.EncryptionService;
+import com.roshan.hungry.helper.JWTHelper;
 import com.roshan.hungry.mapper.CustomerMapper;
 import com.roshan.hungry.repo.CustomerRepo;
 import jakarta.validation.Valid;
@@ -14,13 +16,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
+
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
 
+    /*
     public String createCustomer(CustomerRequest request) {
         Customer customer = mapper.toEntity(request);
         repo.save(customer);
         return "Created";
+    }
+    */
+
+    // private final CustomerRepo customerRepo;
+    // private final CustomerMapper customerMapper;
+    private final EncryptionService encryptionService;
+    private final JWTHelper jwtHelper;
+
+    public String createCustomer(CustomerRequest request) {
+        Customer customer = mapper.toEntity(request);
+        customer.setPassword(encryptionService.encode(customer.getPassword()));
+        repo.save(customer);
+        return "Customer Created Successfully";
     }
 
     public String loginCustomer(CustomerLoginRequest loginRequest) {
